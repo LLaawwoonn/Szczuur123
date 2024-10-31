efa5349e3d635231325886c01ec3a1c6317e222e
mixmg <mixmg1@gmail.com>
2024-10-31 15:32:29 +0100

    addons

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const ffmpeg = require("fluent-ffmpeg");
+const path = require("path");
 const NodeMediaServer = require("node-media-server");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 const config = {
   rtmp: {
@@ -11,11 +20,35 @@ const config = {
   http: {
     port: 8000,
     allow_origin: "*",
-    media_root: "./media",
-    webroot: "./www",
-    api: true,
   },
 };
 
 const nms = new NodeMediaServer(config);
 nms.run();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("videoStream", (data) => {
+    ffmpeg()
+      .input(data)
+      .inputFormat("rawvideo")
+      .outputOptions("-vcodec libx264")
+      .outputOptions("-f flv")
+      .output("rtmp://localhost/live/stream")
+      .run();
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
